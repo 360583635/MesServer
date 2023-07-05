5a3e3b1195c676c33da79aba9b16e68e179d3bb7
cdd <2531349267@qq.com>
2023-07-05 22:29:01 +0800

    cdd

---
+++ Mesbackend/Order-Service/src/main/java/com/job/orderService/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.job.orderService.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.job.orderService.pojo.Order;
+
+public interface OrderService extends IService<Order> {
+}
