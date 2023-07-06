efc1cc899e81e354dab0591f49e5efb4a4cd7153
zhong xin <2565563032@qq.com>
2023-07-06 14:47:14 +0800

    "zx"

---
+++ Mesbackend/Data-Visualization-Service/src/main/java/com/job/dataVisualizationService/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.job.dataVisualizationService.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.job.dataVisualizationService.pojo.Order;
+
+/**
+ * @Author 菜狗
+ */
+public interface OrderService extends IService<Order> {
+    Order getClassification();
+}
