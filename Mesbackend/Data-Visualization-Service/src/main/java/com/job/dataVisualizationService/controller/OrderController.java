d40a4a9e26162a952e5316f61708e85b4466c5b1
zhong xin <2565563032@qq.com>
2023-07-06 17:50:40 +0800

    "zx"

--- Mesbackend/Data-Visualization-Service/src/main/java/com/job/dataVisualizationService/controller/OrderController.java
+++ Mesbackend/Data-Visualization-Service/src/main/java/com/job/dataVisualizationService/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.job.dataVisualizationService.controller;
 
 import com.job.dataVisualizationService.common.result.Result;
+import com.job.dataVisualizationService.mapper.OrderMapper;
 import com.job.dataVisualizationService.pojo.Order;
 import com.job.dataVisualizationService.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+
     @GetMapping("/classification")    //分类占比
     public Result<Order> getClassification(){
-        Order order = orderService.getClassification();
-        return Result.success(new Order(),"success");
+        Order order = new Order();
+        order.setAuditor("zx");
+        System.out.println(1);
+        orderService.save(order);
+        return Result.success(order,"success");
     }
 
-    @GetMapping("/amount")
+    @GetMapping("/amount")   //金额统计
     public Result<Order> getAmount(){
         return Result.success(new Order(),"success");
     }
 
+    @GetMapping("/count")    //数量统计
+    public Result<Order> getCount(){
+        return null;
+    }
 
+    @GetMapping("/prediction/amount")    //金额预测
+    public Result<Order> getPAmount(){
+        return null;
+    }
+
+    @GetMapping("/prediction/count")      //数量预测
+    public Result<Order> getPCount(){
+        return null;
+    }
 }
