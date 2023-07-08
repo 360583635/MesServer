bfd467b86fd60a175c5fc85102ae5c4bcf0b8fd8
username <1570304561@qq.com>
2023-07-08 19:09:59 +0800

    DispatchService

--- Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/mapper/WorkMapper.java
+++
@@ -1,7 +0,0 @@
-package com.job.dispatchService.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.job.common.pojo.Work;
-
-public interface WorkMapper extends BaseMapper<Work> {
-}
