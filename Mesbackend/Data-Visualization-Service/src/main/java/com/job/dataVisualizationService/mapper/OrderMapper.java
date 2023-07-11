2c40398de5d9c181b94d352886b28e2d385190f0
zhong xin <2565563032@qq.com>
2023-07-11 23:41:15 +0800

    "zx"

--- Mesbackend/Data-Visualization-Service/src/main/java/com/job/dataVisualizationService/mapper/OrderMapper.java
+++ Mesbackend/Data-Visualization-Service/src/main/java/com/job/dataVisualizationService/mapper/OrderMapper.java
@@ -1,10 +1,8 @@
 package com.job.dataVisualizationService.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.job.dataVisualizationService.pojo.OrderData;
 import com.job.common.pojo.Order;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 
 /**
