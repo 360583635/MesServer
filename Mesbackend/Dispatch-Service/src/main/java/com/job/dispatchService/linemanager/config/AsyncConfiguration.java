de5a0fecb7a89c8e279025cc9a90946966f83adf
username <1570304561@qq.com>
2023-07-11 23:48:50 +0800

    DispatchService

--- Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/linemanager/config/AsyncConfiguration.java
+++ Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/linemanager/config/AsyncConfiguration.java
@@ -30,9 +30,9 @@ public class AsyncConfiguration implements AsyncConfigurer {
     public ThreadPoolTaskExecutor executor(){
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         //核销线程数
-        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setCorePoolSize(15);
         //线程池维护线程的最大数量,只有在缓冲队列满了之后才会申请超过核心线程数的线程
-        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setMaxPoolSize(15);
         //缓存队列
         taskExecutor.setQueueCapacity(200);
         //允许的空闲时间,当超过了核心线程出之外的线程在空闲时间到达之后会被销毁
