72c11b24699113c23babedf2f3a245ab9e9cff37
test <abc@163.com>
2023-07-17 01:08:28 +0800

    1

--- Mesbackend/Production-Management-Service/src/main/java/com/job/productionManagementService/mapper/MaterialMapper.java
+++ Mesbackend/Production-Management-Service/src/main/java/com/job/productionManagementService/mapper/MaterialMapper.java
@@ -2,6 +2,8 @@ package com.job.productionManagementService.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.job.common.pojo.Equipment;
+import com.job.common.pojo.Material;
 
-public interface EquipmentMapper extends BaseMapper<Equipment> {
+public interface MaterialMapper extends BaseMapper<Material> {
+
 }
