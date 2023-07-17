3bbab7527e2a9b7aba2efafe4e715b00a1d89464
cdd <2531349267@qq.com>
2023-07-17 15:03:01 +0800

    cdd

---
+++ Mesbackend/Order-Service/src/main/java/com/job/orderService/mapper/LineMapper.java
@@ -0,0 +1,14 @@
+package com.job.orderService.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.job.common.pojo.Line;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 庸俗可耐
+ * @create 2023-07-06-17:59
+ * @description
+ */
+@Service
+public interface LineMapper extends BaseMapper<Line> {
+}
