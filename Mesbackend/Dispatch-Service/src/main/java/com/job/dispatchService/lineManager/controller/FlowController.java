8b1278044bb1e8b9701955964cbc3f709b43c3aa
Wen <3237737989@qq.com>
2023-07-19 12:51:12 +0800

    wen

--- Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/lineManager/controller/FlowController.java
+++ Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/lineManager/controller/FlowController.java
@@ -16,6 +16,7 @@ import com.job.dispatchService.lineManager.service.LineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,6 +26,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/dispatch/flow")
+@CrossOrigin
 public class FlowController {
     @Autowired
     public FlowService flowService;
@@ -75,6 +77,12 @@ public class FlowController {
         return Result.error("保存失败");
 
     }
+
+    /**
+     * 根据id删除
+     * @param felowId
+     * @return
+     */
     @PostMapping("removeByid")
     public Result FlowRemove(@RequestParam String felowId){
         LambdaQueryWrapper<Line> queryWrapper=new LambdaQueryWrapper<>();
@@ -86,11 +94,50 @@ public class FlowController {
             return Result.success(null,"删除成功");
         }
     }
+
+    @PostMapping("/batchDelete")
+    @ResponseBody
+    public Result batchDeleteById(@RequestParam List<String> idList){
+
+        // 获取需要逻辑删除的记录的ID列表
+
+        List<Flow> recordList = new ArrayList<>();
+        for (String id : idList) {
+            Flow flow=new Flow();
+            flow.setId(id);
+            flow.setIsDelete(0);  // 设置要更新的字段和值
+            recordList.add(flow);
+        }
+
+        boolean b = flowService.updateBatchById(recordList);
+
+        if(b){
+            return Result.success(null,"查询成功");
+        }
+        return Result.error("删除失败");
+
+
+    }
+
+
+
+    /**
+     * 根据id查询
+     * @param flowId
+     * @return
+     */
     @PostMapping("/queryId")
     public Result flowQueryByid(@RequestParam(value = "flowId") String flowId){
+        if(flowId.isEmpty()){
+            return Result.error("id传输为空，请重新");
+        }
         Flow flow = flowService.getById(flowId);
+        if(flow==null){
+        return Result.error("查询为空，请重试");
+        }
         return Result.success(flow,"查询成功");
     }
+
 
 
 
