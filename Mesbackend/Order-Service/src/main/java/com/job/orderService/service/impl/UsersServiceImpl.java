4b2cd17df19f325214bf7629a18d9070396c0680
cdd <2531349267@qq.com>
2023-07-19 10:15:06 +0800

    cdd

---
+++ Mesbackend/Order-Service/src/main/java/com/job/orderService/service/impl/UsersServiceImpl.java
@@ -0,0 +1,11 @@
+package com.job.orderService.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.job.common.pojo.Users;
+import com.job.orderService.mapper.UsersMapper;
+import com.job.orderService.service.UsersService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements UsersService {
+}
