24c8aa51dfcb17df1dbac99051e3bf2ee938dd9b
cdd <2531349267@qq.com>
2023-07-23 22:14:52 +0800

    cdd

--- Mesbackend/Order-Service/src/main/java/com/job/orderService/service/impl/OrderServiceImpl.java
+++ Mesbackend/Order-Service/src/main/java/com/job/orderService/service/impl/OrderServiceImpl.java
@@ -80,6 +80,17 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
                 && order.getProductName() != null &&  order.getCustomName() !=null && order.getCustomTel() !=null
                 && order.getRawName() !=null && order.getRawNum() !=null && order.getPriority()!=null){
 
+            switch (order.getPriority().toString()){
+                case "普通":
+                    order.setPriority(0);
+                    break;
+                case "较急":
+                    order.setPriority(1);
+                    break;
+                case "紧急":
+                    order.setPriority(2);
+                    break;
+            }
             order.setOrderDate(new Date());
             order.setProductionStatus(0);
             //TODO: 2023/7/8
