fd3b6a409b160bfd54ec9a7f06401f19150fae55
username <1570304561@qq.com>
2023-07-24 15:40:13 +0800

    Dispatchment

--- Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/lineManager/controller/LineTaskController.java
+++ Mesbackend/Dispatch-Service/src/main/java/com/job/dispatchService/lineManager/controller/LineTaskController.java
@@ -26,6 +26,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 /**
@@ -54,9 +55,13 @@ public class LineTaskController {
     @Autowired
     private WorkService workService;
 
+    //1.实例化ReentrantLock
+    private ReentrantLock lock = new ReentrantLock();
+
 
     @Async
     public void lineInstance(Line line) throws InterruptedException{
+
         // TODO: 2023/7/10 流水线实列执行流程
         FlowProcessRelation firstRelation;
         String firstProcessId;
@@ -72,89 +77,95 @@ public class LineTaskController {
                 //订单队列
                 orderQueue = new Vector<Order>(redisCache.getCacheList(lineName));
             }
-            while(true) {
-                //判断订单列表是否有数据，流水线状态是否为 空闲或完成
-                if ((orderQueue.isEmpty() == false && line.getLineStatus().equals("0")) || (orderQueue.isEmpty() == false && line.getLineStatus().equals("4")) ) {
-                    //从订单列表里获取订单
-                    Order order = orderQueue.get(0);
-                    orderQueue.remove(order);
-                    //查询该订单是否有工单关联
-                    LambdaQueryWrapper<Work> queryWrapper = new LambdaQueryWrapper<>();
-                    queryWrapper
-                            .eq(Work::getWState,4)
-                            .eq(Work::getWOrderId, order.getOrderId());
-                    Work work = workService.getOne(queryWrapper);
+                while (true) {
 
-                    flowId = line.getLineFlowId();
-                    if (work != null) {
-                        //该订单曾经被执行过
-                        //接取该订单异常的那个工序
-                        firstProcessId = work.getWProcessId();
-                    } else {
-                        //该订单未被执行
-                        LambdaQueryWrapper<FlowProcessRelation> queryWrapper1 = new LambdaQueryWrapper<>();
-                        queryWrapper1
-                                .eq(FlowProcessRelation::getIsDelete, 1)
-                                .eq(FlowProcessRelation::getFlowId, flowId)
-                                .eq(FlowProcessRelation::getSortNum, "0");
-                        FlowProcessRelation flowProcessRelation = flowProcessRelationService.getOne(queryWrapper1);
-                        firstProcessId = flowProcessRelation.getProcessId();
-                    }
-                    //获取头工序
-                    LambdaQueryWrapper<FlowProcessRelation> queryWrapper2 = new LambdaQueryWrapper<>();
-                    queryWrapper2
-                            .eq(FlowProcessRelation::getIsDelete, 1)
-                            .eq(FlowProcessRelation::getProcessId, firstProcessId)
-                            .eq(FlowProcessRelation::getFlowId, flowId);
-                    firstRelation = flowProcessRelationService.getOne(queryWrapper2);
-                    if (firstRelation != null) {
-                        //将流水线状态修改为繁忙
-                        order.setProductionStatus(2);
-                        line.setLineStatus("2");
-                        FlowProcessRelation relation = firstRelation;
-                        //遍历工序开始生产
-                        while (!StringUtil.isNullOrEmpty(relation.getNextProcessId())) {
-                            //调用工单处理方法
-                            String currentProcessId = relation.getProcessId();
-                            String workingStatus = String.valueOf(workController.working(currentProcessId, order.getOrderId()));
-                            if ("error".equals(workingStatus)) {
-                                //如果工单运行失败
-                                order.setProductionStatus(3);
-                                /*line.setLineStatus("3");*/
-                                line.setExceptionCount(line.getExceptionCount()+1);
-                                // TODO: 2023/7/15 流水线状态为异常后，实时监控工单异常处理信息
+                    try {
+                        lock.lock();
+                        //判断订单列表是否有数据，流水线状态是否为 空闲或完成
+                        if ((orderQueue.isEmpty() == false && line.getLineStatus().equals("0")) || (orderQueue.isEmpty() == false && line.getLineStatus().equals("4"))) {
+                            //从订单列表里获取订单
+                            Order order = orderQueue.get(0);
+                            orderQueue.remove(order);
+                            //查询该订单是否有工单关联
+                            LambdaQueryWrapper<Work> queryWrapper = new LambdaQueryWrapper<>();
+                            queryWrapper
+                                    .eq(Work::getWState, 4)
+                                    .eq(Work::getWOrderId, order.getOrderId());
+                            Work work = workService.getOne(queryWrapper);
 
-                            } else if ("ok".equals(workingStatus)) {
-                                //如果工单运行成功，获取下一个工序
-                                LambdaQueryWrapper<FlowProcessRelation> queryWrapper3 = new LambdaQueryWrapper<>();
-                                queryWrapper3
+                            flowId = line.getLineFlowId();
+                            if (work != null) {
+                                //该订单曾经被执行过
+                                //接取该订单异常的那个工序
+                                firstProcessId = work.getWProcessId();
+                            } else {
+                                //该订单未被执行
+                                LambdaQueryWrapper<FlowProcessRelation> queryWrapper1 = new LambdaQueryWrapper<>();
+                                queryWrapper1
                                         .eq(FlowProcessRelation::getIsDelete, 1)
-                                        .eq(FlowProcessRelation::getProcessId, relation.getNextProcessId())
-                                        .eq(FlowProcessRelation::getFlowId, flowId);
-                                relation = flowProcessRelationService.getOne(queryWrapper3);
+                                        .eq(FlowProcessRelation::getFlowId, flowId)
+                                        .eq(FlowProcessRelation::getSortNum, "0");
+                                FlowProcessRelation flowProcessRelation = flowProcessRelationService.getOne(queryWrapper1);
+                                firstProcessId = flowProcessRelation.getProcessId();
                             }
-                        }
-                        //判断此时的流程是否为最后的流程
-                        if ("lastProcess".equals(relation.getProcessType())) {
-                            //调用工单处理方法
-                            String currentProcessId = relation.getProcessId();
-                            String workingStatus = String.valueOf(workController.working(currentProcessId, order.getOrderId()));
-                            if ("error".equals(workingStatus)) {
-                                //如果工单运行失败，将订单和流水线状态设置为 异常
-                                order.setProductionStatus(3);
-                                /*line.setLineStatus("3");*/
-                                line.setExceptionCount(line.getExceptionCount()+1);
-                            } else if ("ok".equals(workingStatus)) {
-                                //如果工单运行成功，将订单和流水线状态设置为 完成
-                                order.setProductionStatus(4);
-                                line.setLineStatus("4");
-                                line.setSuccessCount(line.getSuccessCount()+1);
-                            }
-                        }
-                    }
+                            //获取头工序
+                            LambdaQueryWrapper<FlowProcessRelation> queryWrapper2 = new LambdaQueryWrapper<>();
+                            queryWrapper2
+                                    .eq(FlowProcessRelation::getIsDelete, 1)
+                                    .eq(FlowProcessRelation::getProcessId, firstProcessId)
+                                    .eq(FlowProcessRelation::getFlowId, flowId);
+                            firstRelation = flowProcessRelationService.getOne(queryWrapper2);
+                            if (firstRelation != null) {
+                                //将流水线状态修改为繁忙
+                                order.setProductionStatus(2);
+                                line.setLineStatus("2");
+                                FlowProcessRelation relation = firstRelation;
+                                //遍历工序开始生产
+                                while (!StringUtil.isNullOrEmpty(relation.getNextProcessId())) {
+                                    //调用工单处理方法
+                                    String currentProcessId = relation.getProcessId();
+                                    String workingStatus = String.valueOf(workController.working(currentProcessId, order.getOrderId()));
+                                    if ("error".equals(workingStatus)) {
+                                        //如果工单运行失败
+                                        order.setProductionStatus(3);
+                                        /*line.setLineStatus("3");*/
+                                        line.setExceptionCount(line.getExceptionCount() + 1);
+                                        // TODO: 2023/7/15 流水线状态为异常后，实时监控工单异常处理信息
 
+                                    } else if ("ok".equals(workingStatus)) {
+                                        //如果工单运行成功，获取下一个工序
+                                        LambdaQueryWrapper<FlowProcessRelation> queryWrapper3 = new LambdaQueryWrapper<>();
+                                        queryWrapper3
+                                                .eq(FlowProcessRelation::getIsDelete, 1)
+                                                .eq(FlowProcessRelation::getProcessId, relation.getNextProcessId())
+                                                .eq(FlowProcessRelation::getFlowId, flowId);
+                                        relation = flowProcessRelationService.getOne(queryWrapper3);
+                                    }
+                                }
+                                //判断此时的流程是否为最后的流程
+                                if ("lastProcess".equals(relation.getProcessType())) {
+                                    //调用工单处理方法
+                                    String currentProcessId = relation.getProcessId();
+                                    String workingStatus = String.valueOf(workController.working(currentProcessId, order.getOrderId()));
+                                    if ("error".equals(workingStatus)) {
+                                        //如果工单运行失败，将订单和流水线状态设置为 异常
+                                        order.setProductionStatus(3);
+                                        /*line.setLineStatus("3");*/
+                                        line.setExceptionCount(line.getExceptionCount() + 1);
+                                    } else if ("ok".equals(workingStatus)) {
+                                        //如果工单运行成功，将订单和流水线状态设置为 完成
+                                        order.setProductionStatus(4);
+                                        line.setLineStatus("4");
+                                        line.setSuccessCount(line.getSuccessCount() + 1);
+                                    }
+                                }
+                            }
+
+                        }
+                    }finally {
+                        lock.unlock();
+                    }
                 }
-            }
         }
     }
 
