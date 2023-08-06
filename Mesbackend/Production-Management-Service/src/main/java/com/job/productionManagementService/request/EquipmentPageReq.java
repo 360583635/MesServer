c5641ec78552bb22789c78696013080c7294a03f
test <abc@163.com>
2023-08-06 15:53:40 +0800

    1

--- Mesbackend/Production-Management-Service/src/main/java/com/job/productionManagementService/request/EquipmentPageReq.java
+++
@@ -1,5 +0,0 @@
-package com.job.productionManagementService.request;
-import com.job.dispatchService.common.BasePageReq;
-public class EquipmentPageReq extends BasePageReq{
-
-}
--- Mesbackend/Production-Management-Service/src/main/java/com/job/productionManagementService/service/WarehouseService.java
+++ Mesbackend/Production-Management-Service/src/main/java/com/job/productionManagementService/service/WarehouseService.java
@@ -2,9 +2,8 @@ package com.job.productionManagementService.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.job.common.pojo.Warehouse;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface WarehouseService extends IService<Warehouse> {
 
 
